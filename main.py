69e24e3f76ff356396a70709c9f85244775b57a0
Ben Hedgepeth <benhh@gmx.com>
2021-07-28 23:02:40 -0700

    Fixed get_show_profile filtering

--- main.py
+++ main.py
@@ -146,28 +146,27 @@ def get_show_profile(connection, show):
     show_param = '%' + show + '%'
     cursor = connection.cursor(dictionary=True)
     cursor.execute(
-        "SELECT title FROM tvshow WHERE title LIKE %(show)s",
+        "SELECT title FROM tvshow WHERE title LIKE %(show)s LIMIT 500",
         {'show': show_param}
     )
     shows = cursor.fetchall()
     while True:
         cls()
-        if len(shows) > 0:
-            print(f"Titles that contain: {show}. Select your show...\n")
-            for i, s in enumerate(shows):
-                print(f"{i + 1} - {s['title']}")
-            _show = input(">>> ")
-            cursor.execute("""
-                SELECT * FROM tvshow WHERE title = %(show)s
-            """, {'show': _show})
-            try:
-                selected_show = cursor.fetchall()[0]
-            except IndexError:
-                print("\nNo show found. Check the title you searched by.")
-                sleep(2)
-                continue
-            show = selected_show
-            break
+        print(f"Titles that contain: {show}. Select your show...\n")
+        for i, s in enumerate(shows):
+            print(f"{i + 1} - {s['title']}")
+        _show = input(">>> ")
+        cursor.execute("""
+            SELECT * FROM tvshow WHERE title = %(show)s
+        """, {'show': _show})
+        try:
+            selected_show = cursor.fetchall()[0]
+        except IndexError:
+            print("\nNo show found. Check the title you searched by.")
+            sleep(2)
+            continue
+        show = selected_show
+        break
     cursor.execute("""
         SELECT t.title, t.view_rating, YEAR(t.release_date) AS release_date,
         t.summary, t.score, t.votes, (
